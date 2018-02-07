ff886fded7f340e20461242c97f5b85f6adb5d01
Marc <marc.oneill2@gmail.com>
2018-02-07 20:07:01 +1100

    Adding swift file

---
+++ cryptotracker/TradeCell.swift
@@ -0,0 +1,58 @@
+//
+//  TradeCell.swift
+//  cryptotracker
+//
+//  Created by Marc O'Neill on 12/01/2018.
+//  Copyright © 2018 marcexmachina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TradeCell: UITableViewCell {
+
+  // MARK: - Outlets
+
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var priceLabel: UILabel!
+  @IBOutlet weak var amountLabel: UILabel!
+
+
+
+  // MARK: - Methods
+
+  func configure(forTrade trade: Trade) {
+    dateLabel.text = timeString(date: trade.date)
+    amountLabel.text = "\(trade.amount)"
+    priceLabel.text = "$\(trade.price)"
+  }
+
+
+  // MARK: - Private Methods
+
+  private func timeString(date: Double) -> String {
+    let date = Date(timeIntervalSince1970: date)
+    let difference = Date().timeIntervalSince(date)
+    let differenceSeconds = Int(difference)
+    let isSeconds = differenceSeconds < 60
+
+    if isSeconds {
+      let isPlural = differenceSeconds > 1 || differenceSeconds == 0
+      if isPlural {
+        return "\(differenceSeconds) seconds ago"
+      } else {
+        return "\(differenceSeconds) second ago"
+      }
+    } else {
+      let minutes = differenceSeconds / 60
+      let differenceMinutes = Int(minutes)
+
+      let isPlural = differenceMinutes > 1
+      if isPlural {
+        return "\(differenceMinutes) minutes ago"
+      } else {
+        return "\(differenceMinutes) minute ago"
+      }
+    }
+  }
+}
