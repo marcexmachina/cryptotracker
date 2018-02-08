b17a8f121eb0cdd75710c9e9928e3bf5c599f6b0
Marc <marc.oneill2@gmail.com>
2018-02-08 16:54:42 +1100

    Delete TradeTableViewCell.swift

--- cryptotracker/TradeTableViewCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TradeTableViewCell.swift
-//  cryptotracker
-//
-//  Created by Marc O'Neill on 12/01/2018.
-//  Copyright © 2018 marcexmachina. All rights reserved.
-//
-
-import Foundation
